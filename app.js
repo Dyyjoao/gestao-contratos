a4a77fed2b65efe288e24df1a6b3494d666f6ab4
Dyyjoao <114023056+Dyyjoao@users.noreply.github.com>
2026-08-29 14:42:32 -0300

    Ativa detalhamento de linhas e drivers contratuais no FP&A

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ import "./js/quotes.js";
 import "./js/fpa.js";
 import "./js/fpa-number-fix.js";
 import "./js/fpa-advanced.js";
+import "./js/planning-details.js";
 import "./js/cashflow.js";
 import "./js/dashboard.js";
 import "./js/permission-ui.js";
