412c9f1107cae80e959929a05dd720d458b2e058
Dyyjoao <114023056+Dyyjoao@users.noreply.github.com>
2026-09-08 17:59:24 -0300

    app: carregar correcoes de workflows

--- app.js
+++ app.js
@@ -22,6 +22,7 @@ import "./js/cashflow-admin-actions.js";
 import "./js/fleet-admin-actions.js";
 import "./js/input-admin-actions.js";
 import "./js/master-admin-actions.js";
+import "./js/workflow-admin-actions.js";
 import "./js/accountability-pdf.js";
 import "./js/permission-ui.js";
 import "./js/polish.js";
