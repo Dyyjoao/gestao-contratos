0c21d5e9c93a20314e62c9181a3f20883afb01b1
Dyyjoao <114023056+Dyyjoao@users.noreply.github.com>
2026-09-09 10:44:18 -0300

    chore: defer contract Storage activation

--- app.js
+++ app.js
@@ -9,7 +9,6 @@ import "./js/profiles.js";
 import "./js/group.js";
 import "./js/contracts.js";
 import "./js/contracts-refresh.js";
-import "./js/contracts-storage.js";
 import "./js/account-reduced-code-ui.js";
 import "./js/management-shell.js";
 import "./js/controllership-router.js";
