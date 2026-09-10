27e75d3c4d31e239daffa6430b7321c340c0930d
Dyyjoao <114023056+Dyyjoao@users.noreply.github.com>
2026-09-10 11:44:04 -0300

    fix: carregar camada visual do cockpit de pagamentos

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ import "./js/payables.js";
 import "./js/payables-contracts.js";
 import "./js/payables-desk.js";
 import "./js/payables-nav.js";
+import "./js/payables-cockpit-ui.js";
 import "./js/account-reduced-code-ui.js";
 import "./js/management-shell.js";
 import "./js/controllership-router.js";
