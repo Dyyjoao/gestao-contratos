ab775fb57d17f44fa9942c74ab0b0cdad901d584
Dyyjoao <114023056+Dyyjoao@users.noreply.github.com>
2026-09-18 15:34:41 -0300

    chore: publicar recarga segura de abastecimentos

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ import "./js/hr-agenda.js";
 import "./js/module-settings.js";
 import "./js/workforce-dashboard.js";
 import "./js/customer-complaints.js";
-import "./js/fleet-fuel.js?v=7";
+import "./js/fleet-fuel.js?v=8";
 import "./js/area-navigation.js";
 import "./js/governance.js";
 import "./js/governance-security.js";
