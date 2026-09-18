d30f8e99519ad1846ba0238a9311df538691981c
Dyyjoao <114023056+Dyyjoao@users.noreply.github.com>
2026-09-18 13:30:10 -0300

    chore: publicar compras de diesel e filtro global

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ import "./js/hr-agenda.js";
 import "./js/module-settings.js";
 import "./js/workforce-dashboard.js";
 import "./js/customer-complaints.js";
-import "./js/fleet-fuel.js";
+import "./js/fleet-fuel.js?v=2";
 import "./js/area-navigation.js";
 import "./js/governance.js";
 import "./js/governance-security.js";
