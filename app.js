316dac9065c577cb02317d328014e51aa3db231e
Dyyjoao <114023056+Dyyjoao@users.noreply.github.com>
2026-09-21 15:32:08 -0300

    chore: publicar auditoria e card de canceladas nas OS

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ import "./js/disposal.js?v=8";
 import "./js/commercial-workflow.js";
 import "./js/pallets.js";
 import "./js/pallet-dashboard.js";
-import "./js/service-orders.js?v=4";
+import "./js/service-orders.js?v=5";
 import "./js/service-orders-dashboard.js?v=2";
 import "./js/workforce-safety.js";
 import "./js/hr-people.js?v=3";
