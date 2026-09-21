ecaf1e6dfa4ba514361d58421aadfd6f17675757
Dyyjoao <114023056+Dyyjoao@users.noreply.github.com>
2026-09-21 13:57:11 -0300

    chore: alinhar registro de funções do RH na produção

--- js/production.js
+++ js/production.js
@@ -1,7 +1,7 @@
 import { abrirPagina, admin } from "./core.js";
 import { $, esc, msg, permite, state, listarDocumentos, criarDocumento, atualizarDocumento, empresaUnicaSelecionadaId, dataBr, emitirAlteracao } from "./shared.js";
 import { confirmarAcaoAdministrativa, atualizarComAuditoria } from "./admin-actions.js";
-import { colaboradoresPorFuncao } from "./hr-role-registry.js?v=7";
+import { colaboradoresPorFuncao } from "./hr-role-registry.js?v=6";
 
 const LEGADO=Object.freeze({
   recurso:["LAJE","MOURÃO","MAQ.1","MAQ.2"],
